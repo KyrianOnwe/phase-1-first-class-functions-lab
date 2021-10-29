8178d1666cb3da9f346befb1952e7b7e639e30f9
kyrian <dr.kyrian.onwe@gmail.com>
2021-10-29 16:13:54 -0500

    commit tree 7459644856493c4f2c6b1f38349c2009b3586068

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+
+function returnFirstTwoDrivers(drivers){
+  const newArray = drivers.slice(0, 2);
+  return newArray;
+}
+function returnLastTwoDrivers(drivers){
+  const backArray = drivers.slice(2);
+  return backArray;
+}
+function selectingDrivers(){
+  const firstArray = returnFirstTwoDrivers();
+  const secondArray = returnLastTwoDrivers();
+  const bigArray = firstArray + secondArray;
+  return bigArray;
+}
